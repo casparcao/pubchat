af32fb28958f0b3b5ad4333c36b0ce08d30f3b57
mike <mike@cao.com>
2025-11-01 21:52:03 +0800

    update

--- user/migrations/11_contact_list.sql
+++ user/migrations/11_contact_list.sql
@@ -1,12 +1,12 @@
-CREATE TABLE `friend` (
+CREATE TABLE `contact` (
   `id` bigint NOT NULL,
-  `user_id` bigint NOT NULL COMMENT '用户ID',
-  `friend_id` bigint NOT NULL COMMENT '好友ID',
+  `here` bigint NOT NULL COMMENT '用户ID',
+  `there` bigint NOT NULL COMMENT '好友ID',
   `status` tinyint NOT NULL DEFAULT '1' COMMENT '关系状态: 1-正常, 2-拉黑',
   `createtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_user_friend` (`user_id`,`friend_id`),
-  KEY `idx_user_id` (`user_id`),
-  KEY `idx_friend_id` (`friend_id`)
+  UNIQUE KEY `uk_here_there` (`here`,`there`),
+  KEY `idx_here` (`here`),
+  KEY `idx_there` (`there`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='好友关系表';
