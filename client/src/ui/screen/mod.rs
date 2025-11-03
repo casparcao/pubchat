9fb14be5279e7e3cd9c603cfeaeaca8d53ae75b2
mike <casparcao@aliyun.com>
2025-11-03 15:38:30 +0800

    update

--- client/src/ui/screen/mod.rs
+++ client/src/ui/screen/mod.rs
@@ -11,16 +11,14 @@ impl App {
         let size = frame.area();
         match &self.view {
             View::Chat { session } => {
-                let chat_screen = ChatScreen::new(&self.token);
                 if self.chat_maximized {
-                    chat_screen.render_maximized_chat_layout(frame, size, session.clone())
+                    self.chat.render_maximized_chat_layout(frame, size, session.clone())
                 } else {
-                    chat_screen.render_main_layout(frame, size, session.clone())
+                    self.chat.render_main_layout(frame, size, session.clone())
                 }
             },
             View::Contact => {
-                let contact_list = ContactListScreen::new(&self.token);
-                contact_list.render_friends_list_layout(frame, size)
+                self.contact.render_friends_list_layout(frame, size)
             },
         }
     }
